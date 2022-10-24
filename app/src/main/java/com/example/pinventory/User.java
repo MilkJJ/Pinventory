4f522149cdfa4f57085e10729030c9da4a20f24e
OJJ <iwannoverify@gmail.com>
2022-10-24 18:00:11 +0800

    User Registration!

---
+++ app/src/main/java/com/example/pinventory/User.java
@@ -0,0 +1,16 @@
+package com.example.pinventory;
+
+public class User {
+
+    public String userName, email;
+
+    public User(){
+
+    }
+
+    public User(String userName, String email){
+        this.userName = userName;
+        this.email = email;
+    }
+
+}
